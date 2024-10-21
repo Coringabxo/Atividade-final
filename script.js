41a7e74862487eb23c1eacc2310b7d3d423a0b4c
Coringabxo <163296210+Coringabxo@users.noreply.github.com>
2024-10-21 08:36:31 -0300

    mudei o codigo

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ document.getElementById("cadastroForm").addEventListener("submit", function(even
   
     
     var nome = document.getElementById("nome").value;
-    var email = document.getElementById("email").value;
+    var  endereço = document.getElementById("endereço").value;
+    var  numero = document.getElementById("numero").value;
 
     var tabela = document.getElementById("cadastros").getElementsByTagName("tbody")[0];
     var novaLinha = tabela.insertRow();
     var celula1 = novaLinha.insertCell(0);
     var celula2 = novaLinha.insertCell(1);
     celula1.innerHTML = nome;
-    celula2.innerHTML = email;
+    celula2.innerHTML = endereço;
+    celula2.innerHTML = numero;
 
     document.getElementById("cadastroForm").reset();
 });
