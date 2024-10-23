af8326ad10809931995411d402b7da797e238fe5
Coringabxo <163296210+Coringabxo@users.noreply.github.com>
2024-10-23 08:21:43 -0300

    arumei

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ document.getElementById("cadastroForm").addEventListener("submit", function(even
   
     
     var nome = document.getElementById("nome").value;
-    var  endereço = document.getElementById("endereço").value;
-    var  numero = document.getElementById("numero").value;
+    var  email = document.getElementById("email").value;
+    ;
 
     var tabela = document.getElementById("cadastros").getElementsByTagName("tbody")[0];
     var novaLinha = tabela.insertRow();
     var celula1 = novaLinha.insertCell(0);
     var celula2 = novaLinha.insertCell(1);
     celula1.innerHTML = nome;
-    celula2.innerHTML = endereço;
-    celula2.innerHTML = numero;
+    celula2.innerHTML = email;
+   
 
     document.getElementById("cadastroForm").reset();
 });
